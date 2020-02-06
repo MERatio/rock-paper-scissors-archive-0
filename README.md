92f6ba5b134c5a33d86dcf2ac513c4a64e9235c7
Mark Edward Ratio <markedwardratio@gmail.com>
2020-02-06 14:54:07 +0800

    Created UI version

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # Rock Paper Scissors Game
-
-Open your developer console to play
--- index.js
+++ index.js
@@ -1,79 +1,59 @@
-// Global variables
-let currentRound = 0;
-const roundLimit = 5;
+// DOM Node References
+const playerScoreDOM = document.querySelector('.player-score-dom');
+const roundCurrentDOM = document.querySelector('.round-current-dom');
+const roundLimitDOM = document.querySelector('.round-limit-dom');
+const computerScoreDOM = document.querySelector('.computer-score-dom');
+const resultText = document.querySelector('.result-text');
+const choices = document.querySelectorAll('.choice');
+const resetGame = document.querySelector('.reset-game');
 
-const scores = {
+// Global variables
+let isGamePlaying = true;
+let roundCurrent = 1;
+let roundLimit;
+let scores = {
   player: 0,
   computer: 0
 };
 
 // Game main function
-function game() {
-  // Play another round
-  if (currentRound < roundLimit) {
-    const playerSelection = getPlayerInput();
+function game(e) {
+  if (!isGamePlaying) return;
+  if (roundCurrent <= roundLimit) {
+    const playerSelection = e.target.id;
     const computerSelection = computerPlay();
 
-    // If the player cancel the prompt
-    if (playerSelection === null) {
-      // If there is a 1 or more games played disply the winner
-      if (currentRound > 0) {
-        console.log(getGameWinner(scores.player, scores.computer));
-        return;
-      } else {
-        return;
-      }
-    }
+    addActiveState(playerSelection);
 
     // Get the round winner
     const roundWinner = getRoundWinner(playerSelection, computerSelection);
 
-    // Display/log round winner and increment score
-    console.log(
-      displayRoundWinner(roundWinner, playerSelection, computerSelection)
-    );
+    // Display round winner and increment score
+    displayRoundWinner(roundWinner, playerSelection, computerSelection);
 
-    // Display/log the scores of both player and computer
-    console.log(getRoundScores(scores.player, scores.computer));
-    currentRound++;
-    game();
+    // Display the scores of both player and computer
+    displayScore(scores.player, scores.computer);
+
+    // If the player plays at the last round, display the winner and stop the game
+    if (roundCurrent === roundLimit) {
+      isGamePlaying = false;
+      removeHoverState();
+      setTimeout(() => {
+        displayGameWinner(scores.player, scores.computer);
+      }, 1500);
+      return;
+    }
+
+    changeRound();
+
     return;
   }
-  // Display/log the winner of the game
-  else {
-    console.log(getGameWinner(scores.player, scores.computer));
-  }
 }
 
-// Get an option for rock, paper, or scissors for do while
-function getOption(prop) {
-  const options = {
-    rock: 'rock',
-    paper: 'paper',
-    scissors: 'scissors'
-  };
-
-  const option = options[prop.toLowerCase()];
-
-  return option ? option : null;
-}
-
-// Get player input using prompt
-function getPlayerInput() {
-  let prop;
-  // Prompt until player gives correct input or the player hit cancel
-  do {
-    prop = prompt('Pick between rock, paper or scissors', '');
-    if (prop === null) return null;
-  } while (!getOption(prop));
-
-  return getOption(prop);
-}
-
-// Get random option between rock, paper, scissors
+// Get random choice between rock, paper, scissors
 function computerPlay() {
-  const options = ['rock', 'paper', 'scissors'];
-  return options[Math.floor(Math.random() * options.length)];
+  const choices = ['rock', 'paper', 'scissors'];
+  return choices[Math.floor(Math.random() * choices.length)];
 }
 
 // Get round winner
@@ -105,34 +85,87 @@ function getRoundWinner(playerSelection, computerSelection) {
 // Display the round winner
 function displayRoundWinner(winner, playerSelection, computerSelection) {
   if (winner === 'draw') {
-    return `Round draw! you picked ${playerSelection}, computer picked ${computerSelection}`;
+    resultText.textContent = `Round draw! you picked ${playerSelection}, computer picked ${computerSelection}`;
+    resultText.style.color = getHTMLPropertyValue('--color-green');
   } else if (winner === 'player') {
     scores.player++;
-    return `You win the round! you picked ${playerSelection}, computer picked ${computerSelection}`;
+    resultText.textContent = `You win the round! you picked ${playerSelection}, computer picked ${computerSelection}`;
+    resultText.style.color = getHTMLPropertyValue('--color-blue');
   } else {
     scores.computer++;
-    return `Computer won the round! you picked ${playerSelection}, computer picked ${computerSelection}`;
+    resultText.textContent = `Computer won the round! you picked ${playerSelection}, computer picked ${computerSelection}`;
+    resultText.style.color = getHTMLPropertyValue('--color-red');
   }
 }
 
-// Get the scores of both player and computer
-function getRoundScores(playerScore, computerScore) {
-  return `Player Score: ${playerScore}, Computer Score: ${computerScore}`;
+// Display the scores of both player and computer
+function displayScore(playerScore, computerScore) {
+  playerScoreDOM.textContent = playerScore;
+  computerScoreDOM.textContent = computerScore;
 }
 
-// Get the winner of the game
-function getGameWinner(playerScore, computerScore) {
-  let winner;
+function changeRound() {
+  roundCurrent++;
+  roundCurrentDOM.textContent = roundCurrent;
+}
+
+// Display the winner of the game
+function displayGameWinner(playerScore, computerScore) {
   if (playerScore === computerScore) {
-    winner = 'The game resulted in a draw!';
+    resultText.textContent = 'The game resulted in a draw!';
+    resultText.style.color = getHTMLPropertyValue('--color-green');
   } else if (playerScore > computerScore) {
-    winner = 'You win the game!';
+    resultText.textContent = 'You win the game!';
+    resultText.style.color = getHTMLPropertyValue('--color-blue');
   } else {
-    winner = 'Computer won the game';
+    resultText.textContent = 'Computer won the game';
+    resultText.style.color = getHTMLPropertyValue('--color-red');
   }
-
-  return winner;
 }
 
-// Play the game
-game();
+function getHTMLPropertyValue(variable) {
+  return window
+    .getComputedStyle(document.documentElement)
+    .getPropertyValue(`${variable}`);
+}
+
+function removeHoverState() {
+  choices.forEach(choice => choice.classList.remove('has-hover'));
+}
+
+function removeActiveState(e) {
+  e.propertyName === 'transform' ? e.target.classList.remove('active') : null;
+}
+
+function addActiveState(playerSelection) {
+  let choice = document.getElementById(playerSelection);
+  choice.classList.add('active');
+  choice.addEventListener('transitionend', removeActiveState);
+}
+
+// Initialization/Reset
+function init() {
+  isGamePlaying = true;
+  roundCurrent = 1;
+  do {
+    roundLimit = +prompt('Input round limit, max = 99', 5) || 5;
+  } while (roundLimit < 2 || roundLimit > 99);
+  let scores = {
+    player: 0,
+    computer: 0
+  };
+  playerScoreDOM.textContent = 0;
+  roundCurrentDOM.textContent = roundCurrent;
+  roundLimitDOM.textContent = roundLimit;
+  computerScoreDOM.textContent = 0;
+  resultText.textContent = 'Select your choice';
+  resultText.style.color = '#333';
+  choices.forEach(choice => choice.classList.add('has-hover'));
+}
+
+init();
+
+resetGame.addEventListener('click', init);
+
+// Start game if the a choice is clicked
+choices.forEach(choice => choice.addEventListener('click', game));
